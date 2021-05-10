a601af921875188e3ecaf67baa22d0764f7091ef
ivana <ivananl@yahoo.com>
2021-05-10 15:31:39 +0200

    Add algoritm for negative key

--- caesar.rb
+++ caesar.rb
@@ -3,14 +3,19 @@
 # rubocop: disable Style/StringChars
 def caesar_cipher(str, num)
   arr = str.split('')
+  num > 26 || num<-26 ? n=num.abs%26 : n=num
   cipher = []
   alphabeth = ('a'..'z').to_a
+  alphabeth = alphabeth.concat(alphabeth)
   alpha = ('A'..'Z').to_a
-  alphabeth.concat(alphabeth).concat(alpha).concat(alpha)
+  alpha = alpha.concat(alpha)
   arr.each do |i|
     if alphabeth.include?(i)
       indx = alphabeth.index { |x| x == i }
-      cipher.push(alphabeth[indx + num])
+      num>=-25 ? cipher.push(alphabeth[indx + n]) : cipher.push(alphabeth[indx - n])
+    elsif alpha.include?(i)
+      indx = alpha.index { |x| x == i }
+      num>=-25 ? cipher.push(alpha[indx + n]) : cipher.push(alpha[indx - n])
     else
       cipher.push(i)
     end
@@ -18,3 +23,5 @@ def caesar_cipher(str, num)
   cipher.join
 end
 # rubocop: enable Style/StringChars
+
+ p caesar_cipher('Mjqqt, Btwqi!', -31)
